a10d725b7f3453864bf7938ffd55d3ca3ebdfbdf
GAMELEIRA <gameleira270499@gmail.com>
2020-02-25 13:11:06 -0300

    Criado o arquivo estruturaRepetitivaFor.js que tẽm como objetivo trabalhar com a estrutura repetitiva for.

---
+++ estruturaRepetitivaFor.js
@@ -0,0 +1,20 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:25/02/2020 
+ * HORA:12:48
+ * FINALIDADE DO PROGRAMA: TRABALHAR COM A ESTRUTURA DE REPETICAO FOR
+ */
+
+    for (let i = 0; i <= 10; i++) {
+
+        console.log(`Contador ${i}`);
+
+    }
+
+const notas = [8.7, 7.4, 9.8, 8.1, 7.7];
+
+for (let i = 0; i < notas.length; i++) {
+
+    console.log(`Notas = ${notas[i]}`);
+
+}
